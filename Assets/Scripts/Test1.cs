c4aa9eb9a5fff22cf1a9cf123a880e02d27c1238
sheepking41 <garyyeung41@gmail.com>
2022-10-13 09:34:31 +0800

    1

---
+++ Assets/Scripts/Test1.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            Debug.Log("Pressed!");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+    }
+}
